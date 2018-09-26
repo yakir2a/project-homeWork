19ea47b1a2f25b68edb477a55570273e569c4d93
Miriam Sade <miriamsade11@gmail.com>
2018-09-26 19:20:50 +0300

    add hidden part

--- Pennywell/src/app/station/station.component.ts
+++ Pennywell/src/app/station/station.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StationComponent implements OnInit {
 
-  stationImg: string;
+ @Input() stationImg: string;
 
   constructor() { }
 
