68fd25495bdfdba9cdf8c7575a0e92ad76d2a74e
yakir2a <yakir2a@gmail.com>
2018-09-26 18:58:04 +0300

    update
    work?

--- Pennywell/src/app/app.component.ts
+++ Pennywell/src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component,OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-<<<<<<< HEAD
 export class AppComponent{
-=======
-export class AppComponent {
->>>>>>> origin/master
 
 }
