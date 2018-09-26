f6c5256ae05ecc7da7b393cd1877c745259381c8
Miriam Sade <miriamsade11@gmail.com>
2018-09-26 19:00:53 +0300

    resoleve conflict

--- Pennywell/src/app/stations.service.ts
+++ Pennywell/src/app/stations.service.ts
@@ -1,43 +1,27 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StationsService {
-
-<<<<<<< HEAD
-  stations : object;
-=======
   stations: object;
->>>>>>> origin/master
-
   constructor() {
     this.stations = [
-<<<<<<< HEAD
-      {name : "Puttin FM" , freq : "66.6" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528391431/music.png"},
-      {name : "Dribbble FM" , freq : "101.2" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528391431/228923-200.png"},
-      {name : "Doge FM" , freq : "99.4" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528456221/MultiDoge.png"},
-      {name : "Ballads FM" , freq : "87.1" , img : "https://res.cloudinary.com/tamirn2/image/upload/v1528456785/Headphone-icon.png"},
-      {name : "Maximum FM" , freq : "142.2" , img : "https://res.cloudinary.com/tamirn2/image/upload/v1528456815/safety-icon.png"},
-      {name : "Puttin FM" , freq : "66.6" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528391431/music.png"},
-      {name : "Dribbble FM" , freq : "101.2" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528391431/228923-200.png"},
-      {name : "Doge FM" , freq : "99.4" , img : "http://res.cloudinary.com/tamirn2/image/upload/v1528456221/MultiDoge.png"},
-      {name : "Ballads FM" , freq : "87.1" , img : "https://res.cloudinary.com/tamirn2/image/upload/v1528456785/Headphone-icon.png"},
-      {name : "Maximum FM" , freq : "142.2" , img : "https://res.cloudinary.com/tamirn2/image/upload/v1528456815/safety-icon.png"}      
-    ];
-  }
-  getStations(){
-    return this.stations;
-=======
       {name : 'Puttin FM' , freq : '66.6' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528391431/music.png'},
       {name : 'Dribbble FM' , freq : '101.2' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528391431/228923-200.png'},
       {name : 'Doge FM' , freq : '99.4' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528456221/MultiDoge.png'},
       {name : 'Ballads FM' , freq : '87.1' , img : 'https://res.cloudinary.com/tamirn2/image/upload/v1528456785/Headphone-icon.png'},
-      {name : 'Maximum FM' , freq : '142.2' , img : 'https://res.cloudinary.com/tamirn2/image/upload/v1528456815/safety-icon.png'}
-  ];
-
->>>>>>> origin/master
+      {name : 'Maximum FM' , freq : '142.2' , img : 'https://res.cloudinary.com/tamirn2/image/upload/v1528456815/safety-icon.png'},
+      {name : 'Puttin FM' , freq : '66.6' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528391431/music.png'},
+      {name : 'Dribbble FM' , freq : '101.2' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528391431/228923-200.png'},
+      {name : 'Doge FM' , freq : '99.4' , img : 'http://res.cloudinary.com/tamirn2/image/upload/v1528456221/MultiDoge.png'},
+      {name : 'Ballads FM' , freq : '87.1' , img : 'https://res.cloudinary.com/tamirn2/image/upload/v1528456785/Headphone-icon.png'},
+      {name : 'Maximum FM' , freq : '142.2' , img : 'https://res.cloudinary.com/tamirn2/image/upload/v1528456815/safety-icon.png'}      
+    ];
+  }
+  getStations() {
+    return this.stations;
   }
 
 }
